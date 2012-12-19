698d4db633e7ec0efbdd49c9052613cab206139a
Ville Tuulos <ville@bitdeli.com>
2012-12-18 21:29:34 -0800

    add repos under the 'repos' key in profile

--- __main__.py
+++ __main__.py
@@ -4,31 +4,32 @@ from datetime import datetime, timedelta
 import json
 import re
 
-REPO_RE = re.compile('https://github.com/.+?/(.+?)(/.*|$)')
+REPO_RE = re.compile('https://github.com/(.+?/.+?)(/.*|$)')
 TFORMAT = '%Y-%m-%dT%H:%M:%SZ'
 RETENTION = params()['plan']['retention-days']
 
 def drop_old(now, profile):
     oldest = (datetime.strptime(now, TFORMAT) -\
               timedelta(days = RETENTION)).strftime(TFORMAT)
-    for key, lst in profile.iteritems():
-        if not key.startswith('!'):
-            i = 0
-            for event in lst:
-                if json.loads(event.data)['tstamp'] > oldest:
-                    break
-                i += 1
-            profile[key] = lst[i:]
+    repos = profile['repos']
+    for key, lst in repos.iteritems():
+        i = 0
+        for event in lst:
+            if json.loads(event.data)['tstamp'] > oldest:
+                break
+            i += 1
+        repos[key] = lst[i:]
     profile.set_expire(RETENTION)
 
 for profile, events in profile_events():
     newest = ''
+    repos = profile.setdefault('repos', {})
     for event in events:
         obj = json.loads(event.object.data)
         repo = REPO_RE.match(obj['referrer']).group(1)
-        if repo in profile:
-            profile[repo].append(event.object)
+        if repo in repos:
+            repos[repo].append(event.object)
         else:
-            profile[repo] = [event.object]
+            repos[repo] = [event.object]
         newest = max(newest, obj['tstamp'])
     drop_old(newest, profile)
